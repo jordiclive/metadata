27ce8a311c19874c7d12a027f8c738689e61eefe
jordiclive <jordiclive19@imperial.ac.uk>
2023-03-02 22:02:17 +0100

    no message

--- slurm_conda2.sh
+++ slurm_conda2.sh
@@ -7,6 +7,7 @@
 #SBATCH --output=%x_%j.out
 source ~/miniconda3/bin/activate meta_conda
 cd /admin/home-jordiclive/metadata
+
 export HF_DATASETS_OFFLINE=1
 export TRANSFORMERS_OFFLINE=1
 # be careful about the cache folder for Wandb
@@ -44,9 +45,8 @@ mixed_precision: fp16
 
 export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-srun bash experiments/hpsearch/test.sh
 
-accelerate launch --config_file accelerate_config.yaml bsmetadata/train.py --config-name v2 --main_process_ip $MASTER_ADDR --main_process_port $MASTER_PORT \
+srun accelerate launch --config_file accelerate_config.yaml bsmetadata/train.py --config-name v2 --main_process_ip $MASTER_ADDR --main_process_port $MASTER_PORT \
   model_name=$MODEL \
     data_config.train_file='*.jsonl.gz' \
     data_config.validation_file='c4-en-html_cc-main-2019-18_pq00-001.jsonl.gz' \
