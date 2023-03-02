eb9be21115e7ee9fc22a9a234b8b509cc6d35a84
jordiclive <jordiclive19@imperial.ac.uk>
2023-03-02 22:17:56 +0100

    no message

--- slurm_conda.sh
+++ slurm_conda.sh
@@ -5,6 +5,7 @@
 #SBATCH --gres=gpu:8
 #SBATCH --ntasks-per-node=8
 #SBATCH --output=%x_%j.out
+source ~/miniconda3/bin/activate meta_conda
 
 export HF_DATASETS_OFFLINE=1
 export TRANSFORMERS_OFFLINE=1
@@ -16,8 +17,7 @@ export HYDRA_FULL_ERROR=1
 export MASTER_PORT=8994
 
 
-source ~/miniconda3/bin/activate meta_conda
 cd /admin/home-jordiclive/metadata
 export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
-export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+export CUDA_VISIBLE_DEVICES="2,3"
 srun bash experiments/hpsearch/test.sh
