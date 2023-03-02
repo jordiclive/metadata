50fc932437a2d0659b5a9124d959c10dcdd89230
jordiclive <jordiclive19@imperial.ac.uk>
2023-03-02 17:18:19 +0100

    no message

---
+++ slurm_script_conda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account laion
+#SBATCH --partition="g40423"
+#SBATCH --job-name=flan
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --output=%x_%j.out
+
+
+
+source ~/miniconda3/bin/activate meta_conda
+cd /admin/home-jordiclive/metadata
+export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+srun bash experiments/hpsearch/test.sh
