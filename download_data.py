36cf655fd42df06d36b36a2c4a48b3275f527d0d
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-05 21:58:59 +0100

    no message

--- download_data.py
+++ download_data.py
@@ -1,10 +1,19 @@
+# import huggingface_hub
+# for k in ['c4-en-html_cc-main-2019-18_pq00-000.jsonl.gz',  'c4-en-html_cc-main-2019-18_pq00-004.jsonl.gz',
+# 'c4-en-html_cc-main-2019-18_pq00-001.jsonl.gz',  'c4-en-html_cc-main-2019-18_pq00-005.jsonl.gz',
+# 'c4-en-html_cc-main-2019-18_pq00-002.jsonl.gz' , 'c4-en-html_cc-main-2019-18_pq01-053.jsonl.gz',
+# 'c4-en-html_cc-main-2019-18_pq00-003.jsonl.gz']:
+#     huggingface_hub.hf_hub_download(repo_id="bs-modeling-metadata/c4-en-html-with-metadata",
+#                                     filename=k, repo_type='dataset',
+#                                     cache_dir='local-data')
+
 import huggingface_hub
-for k in ['c4-en-html_cc-main-2019-18_pq00-000.jsonl.gz',  'c4-en-html_cc-main-2019-18_pq00-004.jsonl.gz',
-'c4-en-html_cc-main-2019-18_pq00-001.jsonl.gz',  'c4-en-html_cc-main-2019-18_pq00-005.jsonl.gz',
-'c4-en-html_cc-main-2019-18_pq00-002.jsonl.gz' , 'c4-en-html_cc-main-2019-18_pq01-053.jsonl.gz',
-'c4-en-html_cc-main-2019-18_pq00-003.jsonl.gz']:
-    huggingface_hub.hf_hub_download(repo_id="bs-modeling-metadata/c4-en-html-with-metadata",
+for k in ['c4-en-html_cc-main-2019-18_pq00-000.jsonl.gz']:
+    huggingface_hub.hf_hub_download(repo_id="bs-modeling-metadata/c4-en-html-with-training_metadata_all",
                                     filename=k, repo_type='dataset',
                                     cache_dir='local-data')
+
+
+
 
 
