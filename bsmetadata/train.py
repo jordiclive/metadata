56b3e908399f6cdc9db288e3a9d78c2612b5bfcf
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-06 14:22:13 +0100

    try like test

--- bsmetadata/train.py
+++ bsmetadata/train.py
@@ -279,7 +279,7 @@ def main(args: CFG) -> None:
         )
         print(new_tokens)
         new_tokens = [
-            AddedToken(token, rstrip=False, lstrip=True, single_word=False, normalized=False) for token in new_tokens
+            AddedToken(token, lstrip=True) for token in new_tokens
         ]
         print(new_tokens)
         tokenizer = AutoTokenizer.from_pretrained(args.model_name, additional_special_tokens=new_tokens)
