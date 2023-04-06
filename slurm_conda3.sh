05ce376591b07c8e0e9307beea183e0725c58fc7
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-06 19:22:45 +0100

    no message

--- slurm_conda3.sh
+++ slurm_conda3.sh
@@ -3,7 +3,8 @@
 #SBATCH --partition="g40"
 #SBATCH --job-name=flan
 #SBATCH --gres=gpu:8
-#SBATCH --ntasks-per-node=12
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=12
 #SBATCH --output=%x_%j.out
 source /fsx/home-jordiclive/miniconda3/bin/activate meta_conda
 cd /fsx/home-jordiclive/metadata
