258cbad512bd23e89c493f090c9eecb34be7696b
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-15 00:10:41 +0100

    no message

--- bsmetadata/experiments/without_metadata.py
+++ bsmetadata/experiments/without_metadata.py
@@ -151,7 +151,7 @@ def build_dataset(tokenizer, args):
     print(f"{len(files_without_entities)} ")
 
     train_files = [x.name for x in files_with_entities if 'c4-en-html_cc-main-2019-18_pq00-000.jsonl.gz' not in x.name]
-
+    train_files = train_files[10:12]
     val_files = [x.name for x in files_with_entities if 'c4-en-html_cc-main-2019-18_pq00-000.jsonl.gz' in x.name]
     # train_files = train_files[:2]
     datasets = load_dataset(path=local_dir,data_files=train_files)
