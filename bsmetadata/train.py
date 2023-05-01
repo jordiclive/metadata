10662e8bfda4b9f17b39bcf9e0521b4ea4c7c4b2
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-01 18:54:14 +0100

    no message

--- bsmetadata/train.py
+++ bsmetadata/train.py
@@ -319,8 +319,8 @@ def main(args: CFG) -> None:
     train_state = TrainState()
 
     # If resume_from_checkpoint_dir is not None, we load the resumed state
-    #todo resume from ckpt
-    path = Path("/fsx/home-jordiclive/tmp/metadata-global/checkpoint-17000step/").resolve()
+    #todo resume from ckpt checkpoint-22000step/global_step22000
+    path = Path("/fsx/home-jordiclive/tmp/metadata-global/checkpoint-22000step/").resolve()
     logger.info(f"Loading checkpoint from {path}")
     if accelerator.distributed_type == DistributedType.DEEPSPEED:
         # this is a deepspeed method, will load model, optimizer, scheduler
